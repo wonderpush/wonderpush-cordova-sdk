3ddb004c68207a7bb9226a30dfe63a67f80e8ee7
Olivier Favre <olivier@wonderpush.com>
2017-11-27 17:09:26 +0100

    iOS support: WIP embeding .frameworks
    Run update-ios-sdk.sh to download the source and build the .frameworks
    for now (ought to be distributed).

---
+++ Podfile
@@ -0,0 +1,4 @@
+target 'WonderPush' do
+    platform :ios, '7.0'
+    pod 'WonderPush', '~> 2.0'
+end
---
+++ update-ios-sdk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# vim: ts=4 sts=4 sw=4 et
+
+git submodule update
+cd wonderpush-ios-sdk
+pod install
+xcodebuild -project Pods/Pods.xcodeproj -scheme WonderPush
+find build -type d -name '*.framework' | while read f; do
+	basename "$f"
+	rm -Rf ../src/ios/"$(basename "$f")"/
+	cp -r "$f" ../src/ios/
+done
