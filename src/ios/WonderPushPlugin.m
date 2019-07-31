397737d14e4ad43cb50e3fe5d7220e6431c6fe39
Olivier Favre <olivier@wonderpush.com>
2019-07-31 10:11:05 +0200

    2.0.2

--- src/ios/WonderPushPlugin.m
+++ src/ios/WonderPushPlugin.m
@@ -35,7 +35,7 @@
         return;
     }
 
-    [WonderPush setIntegrator:@"wonderpush-cordova-sdk-2.0.1"];
+    [WonderPush setIntegrator:@"wonderpush-cordova-sdk-2.0.2"];
 
     NSString *clientId = [self.commandDelegate.settings objectForKey:[@"WONDERPUSH_CLIENT_ID" lowercaseString]];
     NSString *clientSecret = [self.commandDelegate.settings objectForKey:[@"WONDERPUSH_CLIENT_SECRET" lowercaseString]];
