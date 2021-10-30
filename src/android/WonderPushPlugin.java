e55bbcde27203039628db114a9ae992c5791eba8
Olivier Favre <olivier@wonderpush.com>
2021-10-30 08:50:41 +0200

    3.0.10

--- src/android/WonderPushPlugin.java
+++ src/android/WonderPushPlugin.java
@@ -51,7 +51,7 @@ public class WonderPushPlugin extends CordovaPlugin {
 
     @Override
     protected void pluginInitialize() {
-        WonderPush.setIntegrator("wonderpush-cordova-sdk-3.0.9");
+        WonderPush.setIntegrator("wonderpush-cordova-sdk-3.0.10");
 
         // Forward notification clicks and data notifications receipt
         LocalBroadcastManager.getInstance(cordova.getContext()).registerReceiver(new BroadcastReceiver() {
--- src/ios/WonderPushPlugin.m
+++ src/ios/WonderPushPlugin.m
@@ -35,7 +35,7 @@
         return;
     }
 
-    [WonderPush setIntegrator:@"wonderpush-cordova-sdk-3.0.9"];
+    [WonderPush setIntegrator:@"wonderpush-cordova-sdk-3.0.10"];
 
     NSString *clientId = [self.commandDelegate.settings objectForKey:[@"WONDERPUSH_CLIENT_ID" lowercaseString]];
     NSString *clientSecret = [self.commandDelegate.settings objectForKey:[@"WONDERPUSH_CLIENT_SECRET" lowercaseString]];
