4bda5d86500d3ecb23013137b9036cfcc0bfde7f
Stéphane JAIS <stephane@wonderpush.com>
2022-01-27 16:45:30 +0100

    3.0.16

--- src/ios/WonderPushPlugin.m
+++ src/ios/WonderPushPlugin.m
@@ -38,7 +38,7 @@
         return;
     }
 
-    [WonderPush setIntegrator:@"wonderpush-cordova-sdk-3.0.15"];
+    [WonderPush setIntegrator:@"wonderpush-cordova-sdk-3.0.16"];
 
     NSString *clientId = [self.commandDelegate.settings objectForKey:[@"WONDERPUSH_CLIENT_ID" lowercaseString]];
     NSString *clientSecret = [self.commandDelegate.settings objectForKey:[@"WONDERPUSH_CLIENT_SECRET" lowercaseString]];
