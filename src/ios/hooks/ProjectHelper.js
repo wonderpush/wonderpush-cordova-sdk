e3da3992ed0f6c7a953b9884db1eb2673b318486
Stéphane JAIS <stephane@wonderpush.com>
2022-09-06 12:38:05 +0200

    Use iOS SDK 4.1.1

--- src/ios/hooks/ProjectHelper.js
+++ src/ios/hooks/ProjectHelper.js
@@ -335,7 +335,7 @@ class ProjectHelper {
   }
 }
 
-ProjectHelper.POD_VERSION = '4.1.0';
+ProjectHelper.POD_VERSION = '4.1.1';
 ProjectHelper.PODFILE_SNIPPET = "target 'WonderPushNotificationServiceExtension' do\n" +
   "  platform :ios, '10.0'\n" +
   "  use_frameworks!\n" +
