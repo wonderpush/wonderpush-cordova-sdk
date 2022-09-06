c3a5eb3fd2f8c35b1675786d23fbe6b5fadeac0d
Stéphane JAIS <stephane@wonderpush.com>
2022-09-06 16:52:47 +0200

    3.2.2

--- src/android/WonderPushPlugin.java
+++ src/android/WonderPushPlugin.java
@@ -51,7 +51,7 @@ public class WonderPushPlugin extends CordovaPlugin {
 
     @Override
     protected void pluginInitialize() {
-        WonderPush.setIntegrator("wonderpush-cordova-sdk-3.2.1");
+        WonderPush.setIntegrator("wonderpush-cordova-sdk-3.2.2");
 
         // Forward notification clicks and data notifications receipt
         LocalBroadcastManager.getInstance(cordova.getContext()).registerReceiver(new BroadcastReceiver() {
