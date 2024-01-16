b2ba0fd4342693870b4e12714b86c3881191a708
Stéphane JAIS <stephane@wonderpush.com>
2024-01-16 16:19:25 +0100

    3.2.10

--- src/ios/WonderPushPlugin.m
+++ src/ios/WonderPushPlugin.m
@@ -203,7 +203,7 @@ typedef void(^WPURLForDeepLinkCallback)(NSURL *, void (^)(NSURL *));
         [WonderPush setupDelegateForUserNotificationCenter];
     }
 
-    [WonderPush setIntegrator:@"wonderpush-cordova-sdk-3.2.9"];
+    [WonderPush setIntegrator:@"wonderpush-cordova-sdk-3.3.0"];
 
 
     // Here we have no access to launchOptions from application:didFinishLaunchingWithOptions:,
